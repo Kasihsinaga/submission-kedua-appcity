6f9046f55984235a1d3abd61c1cd38006d1c6dc0
Kasihsinaga <sinagakasih130@gmail.com>
2025-11-10 21:28:17 +0700

    Perbaikan Baru

--- src/scripts/pages/home/home-page.js
+++ src/scripts/pages/home/home-page.js
@@ -135,7 +135,7 @@ export default class HomePage {
         return `
           <div class="report-card" style="view-transition-name: card-${s.id};">
             <div class="report-card-content">
-              <h4>${s.name}</h4>
+              <h3>${s.name}</h3>
               <p><b>ID:</b> ${s.id}</p>
               <img src="${s.photoUrl}" alt="${s.name}" />
               <p class="story-date">Tanggal: ${this.#presenter.formatDate(s.createdAt)}</p>
